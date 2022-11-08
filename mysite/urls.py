87c3105d4b48e6c1313379ca6a11bf61997ea55d
Sumon Dey <deysumon99@gmail.com>
2022-11-08 19:09:43 +0530

    used templates and views

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from polls import views
+app_name = 'polls'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('owner/', views.owner, name='owner'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]

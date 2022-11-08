3f1ae24c5cb2ecad15c5892fe4bc54f8f5ae5746
Sumon Dey <deysumon99@gmail.com>
2022-11-08 19:43:28 +0530

    Made Complete Score Web Project

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,9 +19,15 @@ from polls import views
 app_name = 'polls'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('owner/', views.owner, name='owner'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    # path('', views.index, name='index'),
+    # path('owner/', views.owner, name='owner'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('owner', views.owner, name='owner'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
